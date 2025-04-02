e0fb013d7c9a4f04ea117e9e0e6af57eec8f760e
výuka <desmeand@cf-frontend03-prod.cls.in.fit.cvut.cz>
2025-04-02 04:58:22 +0200

    Added slurm file for task-parallelism

---
+++ task_cluster.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Setting the control parameters of the Slurm scheduler
+#SBATCH --job-name=run_task
+#SBATCH --output="%x-%J.out"
+#SBATCH --error="%x-%J.err"
+
+# Activation of HPE CPE
+source /etc/profile.d/zz-cray-pe.sh
+
+# Setting environment variables for the scheduled task
+#module load cray-mvapich2_pmix_nogpu/2.3.7
+module load cray-mvapich2_pmix_nogpu
+
+# Access the number of cores allocated per task (from the -c parameter)
+CORES_PER_TASK=$SLURM_CPUS_PER_TASK
+
+# After the srun command, write the path to your program and its arguments for running on the scheduled computing nodes:
+srun ./build/program $1 $CORES_PER_TASK
+
+exit 0
